a92a70f1d02ae20a73ba2639039a0893f7c619b5
Kelby <green.kelby@gmail.com>
2014-07-30 19:28:24 -0400

    refactor so updates start immediately

--- sign.rb
+++ sign.rb
@@ -4,8 +4,7 @@ require 'timers'
 require 'thread'
 
 class Sign
-  @first_refresh = true
-  @scores = {}
+  @scores = nil
   @current_index = 0
   @lock = Mutex.new
 
@@ -46,38 +45,44 @@ class Sign
     puts bitmap[:data].scan(/.{#{bitmap[:width]}}|.+/).join("\n")
   end
 
+  def self.refresh_scores
+    @lock.synchronize do
+      @scores = SportsFeed.get_scores
+    end
+  end
+
+  def self.update_sign
+    @lock.synchronize do
+      if !@scores.nil? && @scores[:num] > 0
+        @current_index %= @scores[:num]
+        i = 0
+        @scores[:leagues].each do |league_name, league_scores|
+          league_scores.each do |score|
+            if i == @current_index
+              write_to_sign(get_bitmap_for_score(league_name, score))
+            end
+            i += 1
+          end
+        end
+      end
+      @current_index += 1
+    end
+  end
+
   def self.run
     scores_refresh_time = 30
     display_refresh_time = 5
 
     timers = Timers::Group.new
 
+    refresh_scores
     timers.every(scores_refresh_time) do
-      @lock.synchronize do
-        @scores = SportsFeed.get_scores
+      refresh_scores
+    end
 
-        if @first_refresh
-          timers.every(display_refresh_time) do
-            @lock.synchronize do
-              if @scores[:num] > 0
-                @current_index %= @scores[:num]
-                i = 0
-                puts @scores.inspect
-                @scores[:leagues].each do |league_name, league_scores|
-                  league_scores.each do |score|
-                    if i == @current_index
-                      write_to_sign(get_bitmap_for_score(league_name, score))
-                    end
-                    i += 1
-                  end
-                end
-              end
-              @current_index += 1
-            end
-          end
-        end
-        @first_refresh = false
-      end
+    update_sign
+    timers.every(display_refresh_time) do
+      update_sign
     end
 
     loop { timers.wait }
