c96714b685b96e3f6b1bcdc2da16d6c8d1d02ee6
Kelby <green.kelby@gmail.com>
2014-08-07 16:21:44 -0400

    remove news api as espn is discontinuing it

--- sports_feed.rb
+++ sports_feed.rb
@@ -3,59 +3,6 @@ require 'json'
 require 'cgi'
 
 class SportsFeed
-  def self.get_local_headlines
-    feeds = {'Cubs' => 'mlb:16',
-             'Bulls' => 'nba:4',
-             'Blackhawks' => 'nhl:4',
-             'Bears' => 'nfl:3',
-            }
-
-    api_key = 'fd3998mu4gmyx2jffg66vkjq'
-
-    headlines = {}
-
-    feeds.each do |team_name, feed|
-      league = feed.split(':')[0]
-      team_id = feed.split(':')[1]
-      url = "http://api.espn.com/v1/now?leagues=#{league}&teams=#{team_id}&content=story&apikey=fd3998mu4gmyx2jffg66vkjq"
-
-      response = {}
-      while response['feed'].nil?
-        response = JSON.parse(Net::HTTP.get_response(URI.parse(url)).body)
-      end
-
-      team_headlines = []
-      response['feed'].each do |story|
-        if story['type'] == 'HeadlineNews'
-          team_headlines << story['description']
-        end
-      end
-
-      headlines[team_name] = team_headlines
-    end
-
-    headlines
-  end
-
-  def self.get_top_headlines
-    api_key = 'fd3998mu4gmyx2jffg66vkjq'
-    headlines = []
-    url = "http://api.espn.com/v1/now/top?content=story&apikey=fd3998mu4gmyx2jffg66vkjq"
-
-    response = {}
-    while response['feed'].nil?
-      response = JSON.parse(Net::HTTP.get_response(URI.parse(url)).body)
-    end
-
-    response['feed'].each do |story|
-      if story['type'] == 'HeadlineNews'
-        headlines << story['description']
-      end
-    end
-
-    headlines
-  end
-
   def self.get_scores
     feeds = {'NFL' => 'nfl',
              'NBA' => 'nba',
