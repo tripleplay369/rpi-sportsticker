6c9d904e381f0a0f8f7cb31fb9249177e052e2f6
Kelby <green.kelby@gmail.com>
2014-08-16 15:07:56 -0400

    try to make sure only one instance is running

---
+++ run_script.sh
@@ -0,0 +1,13 @@
+LOCKFILE=/tmp/lock.txt
+if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
+    echo "already running"
+    exit
+fi
+
+# make sure the lockfile is removed when we exit and then claim it
+trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+echo $$ > ${LOCKFILE}
+
+ruby sign.rb
+
+rm -f ${LOCKFILE}
